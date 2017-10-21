150869e2ea8e8949c534f05d8f7903053984a303
Rafal Woloszyn <woloszyn.rafal@gmail.com>
2017-10-21 21:14:54 +0200

    Add web interface with Flask

--- python_sdcopy.py
+++ python_sdcopy.py
@@ -1,6 +1,24 @@
 import os, os.path
 import subprocess
+import time
+import signal
+
 from subprocess import Popen
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+@app.route("/run_copy")
+def run_copy():
+    copy_card('testfile.txt')
+
+    return "Copy done !"
 
 
 def copy_card(file_name):
@@ -10,6 +28,18 @@ def copy_card(file_name):
 
     result = Popen('dd if=' + file_name + ' | pv -s  ' + str(file_size) + ' | dd of=testfile2.txt bs=4M', shell=True,
                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    while (result.poll()) is None:
+        time.sleep(.3)
+        result.send_signal(signal.SIGUSR1)
+        # while 1:
+        #     l = result.stderr.readline()
+        #     if 'records in' in l:
+        #         print
+        #         l[:l.index('+')], 'records',
+        #     if 'bytes' in l:
+        #         print
+        #         l.strip(), '\r',
+        # break
 
     print(str(result.stderr))
 
@@ -18,7 +48,8 @@ def copy_card(file_name):
 
 
 def main():
-    copy_card('testfile.txt')
+    app.debug = True
+    app.run(use_reloader=False, host='0.0.0.0')
 
 
 if __name__ == '__main__':
